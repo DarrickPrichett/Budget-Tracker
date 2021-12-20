0c7f3b7257ab0e5cc623039cd8bee5d140905cdc
DarrickPrichett <darrickprichett@gmail.com>
2021-12-19 23:32:29 -0600

    complete code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
-const APP_PREFIX = 'FoodEvent-';     
+const APP_PREFIX = 'Budget-Tracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "./index.html",
-  "./css/style.css",
+  "./css/styles.css",
   "./manifest.json",
   "./js/idb.js",
   "./js/index.js",
